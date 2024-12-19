858d01ea16edeaa51f938f9862165771c410ab5c
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-19 23:46:29 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -17,7 +17,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/Gokukhan1/musicgoku"
+                        "ƨσʋяcɛ", url=f"https://github.com/Gokukhan1/musicgoku/fork"
                     )
                 ]
             ]
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/Gokukhan1/musicgoku"
+                        "ƨσʋяcɛ", url=f"https://github.com/Gokukhan1/musicgoku/fork"
                     )
                 ]
             ]
