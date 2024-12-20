c0abbbd6cd651fd3b8f8bec1f4d665863a072f19
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-20 11:42:47 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -28,7 +28,7 @@ from strings import get_string
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    await message.react("🥀")
+    await message.react("❤")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
