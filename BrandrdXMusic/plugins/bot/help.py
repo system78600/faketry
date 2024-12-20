86a17e57944590547f7ad7451259b51de8e797c4
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-20 17:59:44 +0530

    Update help.py

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -91,7 +91,9 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
     elif cb == "hb15":
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
-    
+    elif cb == "hb16":
+        await CallbackQuery.edit_message_text(helpers.HELP_16, reply_markup=keyboard)
+        
 
 @app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
