06941775d9018ab2cca3b8c96d8617d33758a1eb
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-22 18:26:54 +0530

    Update help.py

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -103,6 +103,10 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_25"],
                     callback_data="help_callback hb16",
                 ),
+                InlineKeyboardButton(
+                    "fun game",
+                    callback_data="help_callback hb21",
+                ),
             ],
             [   InlineKeyboardButton(
                     text=_["H_B_27"],
