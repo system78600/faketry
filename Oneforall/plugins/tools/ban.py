86d6df8c3ceb9cdbe1903e656be0fdf73d90e08f
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2025-02-01 01:49:58 +0530

    Update ban.py

--- Oneforall/plugins/tools/ban.py
+++ Oneforall/plugins/tools/ban.py
@@ -25,7 +25,7 @@ from Oneforall.utils.functions import (
     extract_user_and_reason,
     time_converter,
 )
-from utils.permissions import adminsOnly, member_permissions
+from Oneforall.utils.permissions import adminsOnly, member_permissions
 from config import BANNED_USERS
 
 warnsdb = mongodb.warns
