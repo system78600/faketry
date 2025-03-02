f4ed4df0768e83a9f462a95c0a0cdb1512622a50
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2025-03-03 02:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gokukhan1/musicgoku",
+    "https://github.com/Gokukhan1/faketry",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
