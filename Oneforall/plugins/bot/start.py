202dbde72e82bfe46fc375234a3788940e978257
system78600 <uzefkhatri552@gmail.com>
2025-05-06 19:03:34 +0530

    Update start.py

--- Oneforall/plugins/bot/start.py
+++ Oneforall/plugins/bot/start.py
@@ -34,7 +34,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             await message.reply_sticker(
-                "CAACAgUAAyEFAASOVshSAAIsmWdpHh2gKv710eialDAePRGVS03fAAJEDwACHPRIV-Jt5CoTZx1QNgQ"
+                "CAACAgUAAxkBAAIVFGgaCNqKGqfvU_d-bl-49UjDvZNYAALOEAAC7aKJVJ51IEGPiWO1HgQ"
             )
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
